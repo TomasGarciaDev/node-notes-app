23b4cc13431abffca83942b4a051618b6e0b9dd1
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-01-07 12:33:14 +0100

    CRUD: create

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+import { insertDB, saveDB, getDB } from "./db";
+
+export const newNote = async (note, tags) => {
+  const newNote = { tags, id: Date.now(), content: note };
+
+  await insertDB(newNote);
+  return newNote;
+};
+
+export const getAllNotes = async () => {
+  const { notes } = await getDB();
+  return notes;
+};
