0ef91f49f1a5d08b9bebb2fafa1ad969e241ec52
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-02-04 16:13:32 +0100

    Feat: Vanilla js server

--- src/command.js
+++ src/command.js
@@ -7,6 +7,7 @@ import {
   removeNote,
   removeAllNotes,
 } from "./notes.js";
+import { start } from "./server.js";
 
 const listNotes = (notes) => {
   notes.forEach(({ id, content, tags }) => {
@@ -86,7 +87,10 @@ yargs(hideBin(process.argv))
         type: "number",
       });
     },
-    async (argv) => {}
+    async (argv) => {
+      const notes = await getAllNotes();
+      start(notes, argv.port);
+    }
   )
   .command(
     "clean",
